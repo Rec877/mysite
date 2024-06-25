3900e095b6e6015f193988d42e440bf525fe3d3c
Rec877 <bananarec877@gmail.com>
2024-06-25 10:31:43 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let result = confirm('カメムシにぜひ寄付してね');
+
+if (result) {
+alert("ありがと♡");
+} else {
+alert("は？お前何を四天王？");
+alert("許せんなぁ、許せんなぁ！！");
+let result = confirm('こういうときはなんていうのかなぁ？');
+
+if (result) {
+alert("やっぱり君は賢いね！");
+} else {
+alert("...この低能め！");
+}}
